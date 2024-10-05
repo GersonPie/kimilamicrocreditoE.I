63e8e94837b1cbd40394d7f67934f9866739a3c9
gersonPie <info.gersontamele@gmail.com>
2024-10-06 00:49:02 +0200

    fixes UI

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
+import App, { AppContext } from '../App'
 
 export const NavBar = () => {
+
+    const {go_to_page} = useContext(AppContext)
   return (
     <div className="navbar">
         <div className="navbar-each-icon-wrapper">
-            <img src={assets.icon_home} alt="" />
+            <img src={assets.icon_home} onClick={
+                ()=>{
+                    go_to_page("boot")
+                }
+            } alt="" />
         </div>
 
         <div className="navbar-each-icon-wrapper">
-            <img src={assets.icon_people} alt="" />
+            <img src={assets.icon_people} onClick={
+                ()=>{
+                    go_to_page("clients")
+                }
+            } alt="" />
         </div>
 
         <div className="navbar-each-icon-wrapper">
-            <img src={assets.icon_add} alt="" />
+            <img src={assets.icon_add} onClick={
+                ()=>{
+                    go_to_page("add_client")
+                }
+            } alt="" />
         </div>
 
         <div className="navbar-each-icon-wrapper">
-            <img src={assets.icon_graph} alt="" />
+            <img src={assets.icon_graph} onClick={
+                ()=>{
+                    go_to_page("graph")
+                }
+            } alt="" />
         </div>
 
         <div className="navbar-each-icon-wrapper">
